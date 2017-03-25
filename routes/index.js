61c9b3d1ec4e443661e61df91968c1ebfae2f5f7
Kelvin <kelvin60429@gmail.com>
2017-03-25 03:14:21 +0000

    added api endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'ToDoExpress' });
+});
+
+/* GET task list */
+router.get('/tasks', function(req, res) {
+});
+
+/* POST create task */
+router.post('/tasks', function(req, res) {
+});
+
+/* GET task detail */
+router.get('/tasks/:taskId', function(req, res) {
+});
+
+/* DELETE task by id */
+router.delete('/tasks/:taskId', function(req, res) {
 });
 
 module.exports = router;
